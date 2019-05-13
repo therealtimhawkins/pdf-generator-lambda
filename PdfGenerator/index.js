c2816197117fe751f00cd9f4cc14b0638cc1e734
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-05-13 13:57:45 +0100

    adds gitignore

--- PdfGenerator/index.js
+++ PdfGenerator/index.js
@@ -5,13 +5,15 @@ process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'
 
 exports.handler = function(event, context, callback) {
 
+  console.log(event.htmlDoc);
+
   wkhtmltopdf(event.htmlDoc, { pageSize: 'letter' }, (error) => {
     if (error) {
       callback(error);
       return;
     }
   });
-  
+
   callback(null, {
     message: 'Test is working'
   });
