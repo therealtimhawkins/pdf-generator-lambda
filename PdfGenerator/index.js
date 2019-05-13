936eb1836725762fffcebd0da3f41108123aebe2
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-05-13 14:17:03 +0100

    adds logging

--- PdfGenerator/index.js
+++ PdfGenerator/index.js
@@ -5,13 +5,14 @@ process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'
 
 exports.handler = function(event, context, callback) {
 
-  console.log(event.htmlDoc);
-
   wkhtmltopdf(event.htmlDoc, { pageSize: 'letter' }, (error) => {
     if (error) {
+      console.log('Failed to convert html to pdf...');
       callback(error);
       return;
     }
+
+    console.log('Pdf was generated successfully!');
     callback(null, {
       message: 'Test is working'
     });
