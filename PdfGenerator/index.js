505f214512d1bfa6ca936f5c03edeba8730495e2
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-05-13 14:22:32 +0100

    adds Buffer to convert base64 to string

--- PdfGenerator/index.js
+++ PdfGenerator/index.js
@@ -4,15 +4,15 @@ const wkhtmltopdf = require('wkhtmltopdf');
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
 exports.handler = function(event, context, callback) {
+  const html_utf8 = new Buffer(event.htmlDoc, 'base64').toString('utf8');
 
-  wkhtmltopdf(event.htmlDoc, { pageSize: 'letter' }, (error) => {
+  wkhtmltopdf(html_utf8, { pageSize: 'letter' }, (error) => {
     if (error) {
       console.log('Failed to convert html to pdf...');
       callback(error);
       return;
-    }
-
-    console.log('Pdf was generated successfully!');
+    };
+    console.log('Pdf was generated successfully...');
     callback(null, {
       message: 'Test is working'
     });
