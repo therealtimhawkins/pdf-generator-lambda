c3526095eda732b0af7a1dfbb0e5baf4c1839a63
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-05-13 13:47:24 +0100

    sets up lambda test

--- PdfGenerator/index.js
+++ PdfGenerator/index.js
@@ -4,10 +4,15 @@ const wkhtmltopdf = require('wkhtmltopdf');
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
 exports.handler = function(event, context, callback) {
+
   wkhtmltopdf(event.htmlDoc, { pageSize: 'letter' }, (error) => {
     if (error) {
       callback(error);
       return;
     }
-  }).pipe(fs.createWriteStream('output.pdf'));;
+  });
+  
+  callback(null, {
+    message: 'Test is working'
+  });
 };
